d593ffab8bb6f02f6d7020a42ad41eb417f1e6b4
jmmorillo <j.e.m@live.com.ar>
2015-04-08 23:15:40 -0300

    TP #0
    Archivos hello.cpp y salida.txt

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+/* Programa de Hola Mundo
+* Juan Manuel Morillo Montero
+* 04-04-2015
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hola Juan Manuel"<<endl;
+    
+    system("PAUSE");
+    return 0;
+}
